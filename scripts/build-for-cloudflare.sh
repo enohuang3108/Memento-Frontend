44246eb3590d41bb81a66004690f9635deb482f7
eno.huang <enohuang3108@gmail.com>
2025-11-29 12:48:54 +0800

    fix: 增加 build-for-cloudflare.sh 中的環境變數輸出

--- scripts/build-for-cloudflare.sh
+++ scripts/build-for-cloudflare.sh
@@ -2,6 +2,11 @@
 
 BRANCH="$CF_PAGES_COMMIT_REF"
 
+echo "CF_PAGES_BRANCH: $CF_PAGES_BRANCH"
+echo "CF_PAGES_COMMIT_REF: $CF_PAGES_COMMIT_REF"
+echo "CF_PAGES_GIT_BRANCH: $CF_PAGES_GIT_BRANCH"
+echo "GITHUB_REF_NAME: $GITHUB_REF_NAME"
+
 if [ "$BRANCH" = "main" ] || [ "$BRANCH" = "master" ]; then
   echo "Building for production (branch: $BRANCH)"
   pnpm run build:production
